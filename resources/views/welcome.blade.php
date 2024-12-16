2bbbc1fa325b086cbbfb71505eb6bd6e285c4bf2
Sebastian <juansecapri@hotmail.com>
2024-12-15 19:25:42 -0500

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Regreso al Cole</title>
+</head>
+<body>
+    <h1>Bienvenido a Regreso al Cole</h1>
+</body>
+</html>
