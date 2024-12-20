b7d1fb1c2151a36a1206a6eb5877d268a725168c
Sebastian <juansecapri@hotmail.com>
2024-12-20 01:22:04 -0500

    Ajustes ultima version

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -24,6 +24,25 @@
         {{ $slot }}
     </main>
     
-    
+    <script>
+        window.addEventListener('mouseover', initLandbot, { once: true });
+        window.addEventListener('touchstart', initLandbot, { once: true });
+        var myLandbot;
+        function initLandbot() {
+            if (!myLandbot) {
+                var s = document.createElement('script');
+                s.type = "module"
+                s.async = true;
+                s.addEventListener('load', function() {
+                var myLandbot = new Landbot.Livechat({
+                    configUrl: 'https://storage.googleapis.com/landbot.pro/v3/H-2725119-8US3G5QD0NJJVDYQ/index.json',
+                });
+                });
+                s.src = 'https://cdn.landbot.io/landbot-3/landbot-3.0.0.mjs';
+                var x = document.getElementsByTagName('script')[0];
+                x.parentNode.insertBefore(s, x);
+            }
+        }
+    </script>
 </body>
-</html>
+</html>y
--- resources/views/livewire/dashboard/galeria.blade.php
+++ resources/views/livewire/dashboard/galeria.blade.php
@@ -2,27 +2,10 @@
 <div>
     <h1>Galería</h1>
     <div class="galeria">
-        @foreach($fotosPortada as $foto)
+        @foreach ($fotosPortada as $foto)
             <div class="galeria-item">
-                <img src="{{ Storage::url($foto) }}" alt="Foto de Portada">
+                <img class="galeria-img" src="{{ Storage::url($foto) }}" alt="Foto de Portada">
             </div>
         @endforeach
     </div>
-
-    <style>
-        .galeria {
-            display: flex;
-            flex-wrap: wrap;
-            gap: 10px;
-        }
-        .galeria-item {
-            flex: 1 1 calc(33.333% - 10px);
-            box-sizing: border-box;
-        }
-        .galeria-item img {
-            width: 100%;
-            height: auto;
-            display: block;
-        }
-    </style>
 </div>
