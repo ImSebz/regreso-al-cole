27f47d3a2be43d25f18d1780e03c7f6be3c3e596
Sebastian <juansecapri@hotmail.com>
2024-12-20 00:34:34 -0500

    Avances plataforma

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -3,16 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mi Aplicación</title>
+    <link rel="icon" href="{{ asset('assets/favicon.ico') }}">
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/header.css') }}">
+    <title>Regreso al cole</title>
 </head>
 <body>
     @auth
-    <header>
-        <h1>Mi Aplicación</h1>
+    <header class="main-header">
         <nav>
             <a href="{{ route('registro-compra') }}">Registro Compra</a>
-            <a href="{{ route('galeria') }}">Galería</a>
             <a href="{{ route('ganadores') }}">Ganadores</a>
+            <a href="{{ route('galeria') }}">Galería</a>
+            <span>{{ Auth::user()->name }}</span>
         </nav>
     </header>
     @endauth
