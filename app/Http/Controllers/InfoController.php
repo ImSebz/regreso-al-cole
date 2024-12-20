7b87cb755808bc4ad5897a1466ca6a1cad78b472
Neffer2 <n3f73r@gmail.com>
2024-12-20 00:05:56 -0500

    1. Infocontroller

---
+++ app/Http/Controllers/InfoController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class InfoController extends Controller
+{
+    public function getUser($documento){
+        $user = User::where('cedula', $documento)->first();
+
+        return response()->json($user);
+    }
+} 
