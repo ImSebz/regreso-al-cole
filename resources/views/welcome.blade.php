f0076116f4d59b153a52eb8dbe84bcbd5e1fcbd0
Sebastián Camargo <juansecapri@hotmail.com>
2024-12-26 11:08:22 -0500

    Avances en artes de la plataforma

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="{{ asset('assets/favicon.ico') }}">
+    <link rel="stylesheet" href="{{ asset('css/welcome.css') }}">
     <title>Regreso al Cole</title>
 </head>
+
 <body>
-    <h1>Bienvenido a Promo regreso al cole</h1>
-    <p>Regístrate para acceder a todos los beneficios</p>
-    <a href="{{ route('register') }}">Regístrate</a>
-    <a href="{{ route('login') }}">Iniciar Sesión</a>
+    <div class="welcome-container">
+        <div class="welcome-items">
+            <div class="welcome-logo">
+                <div class="top-logo">
+                    <img src="{{ asset('assets/expresa-tus-colores-logo.png') }}" alt="">
+                </div>
+                <div class="bot-logo">
+                    <img src="{{ asset('assets/sharpie-logo.png') }}" alt="">
+                    <img src="{{ asset('assets/papermate-logo.png') }}" alt="">
+                    <img src="{{ asset('assets/prismacolor-logo.png') }}" alt="">
+                </div>
+            </div>
+
+            <div class="welcome-info-container">
+                <div class="welcome-info-text">
+                    <h1 class="welcome-title">¿Cómo participar?</h1>
+                    <div class="welcome-info">
+                        <p>1. Regístrate.</p>
+                        <p>2. Sube la factura de compra con la foto de los productos participantes más tu dibujo.</p>
+                        <p>3. Podrás ser uno de los 150 ganadores semanales.</p>
+                    </div>
+                    <div class="welcome-btn-container">
+                        <a class="welcome-btn" id="entrar_btn" href="{{ route('login') }}">Entra</a>
+                        <a class="welcome-btn" id="registrar_btn" href="{{ route('register') }}">Registrate</a>
+                    </div>
+                </div>
+                <div class="welcome-premios-img">
+                    <img src="{{ asset('assets/premios-home-dekstop.png') }}" alt="">
+                </div>
+
+            </div>
+        </div>
+    </div>
 
 </body>
+
 </html>
