221cbc73f80bfc1732fdea19fc202b60acac8fc7
Sebastián Camargo <juansecapri@hotmail.com>
2024-12-27 11:31:21 -0500

    Tyc y tratamiento de datos agregado

--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -1,16 +1,16 @@
 <div class="register-container">
     <div class="register-box">
         <form wire:submit.prevent="register">
-            <input type="text" wire:model="name" placeholder="Name">
+            <input type="text" wire:model="name" placeholder="Nombre Completo">
             @error('name')
                 <span>{{ $message }}</span>
             @enderror
-    
+
             <input type="text" wire:model="cedula" placeholder="Cedula">
             @error('cedula')
                 <span>{{ $message }}</span>
             @enderror
-    
+
             <div class="input-cont">
                 <label for="departamento">Departamento: </label>
                 <select id="departamento" wire:model.live="departamento" class="select-departamento">
@@ -41,40 +41,44 @@
                     </div>
                 @enderror
             </div>
-            <input type="email" wire:model="email" placeholder="Email">
+            <input type="email" wire:model="email" placeholder="Correo">
             @error('email')
                 <span>{{ $message }}</span>
             @enderror
-    
+
             <input type="text" wire:model="celular" placeholder="Celular">
             @error('celular')
                 <span>{{ $message }}</span>
             @enderror
-    
-            <input type="password" wire:model="password" placeholder="Password">
+
+            <input type="password" wire:model="password" placeholder="Contraseña">
             @error('password')
                 <span>{{ $message }}</span>
             @enderror
-    
-            <input type="password" wire:model="password_confirmation" placeholder="Confirm Password">
-    
+
+            <input type="password" wire:model="password_confirmation" placeholder="Confirma Contraseña">
+
             <input type="date" wire:model="fecha_nacimiento" placeholder="Fecha de Nacimiento">
             @error('fecha_nacimiento')
                 <span>{{ $message }}</span>
             @enderror
-    
-            <input type="checkbox" wire:model="aceptar_tyc"> Aceptar Términos y Condiciones
+
+            <input type="checkbox" wire:model="aceptar_tyc"> Aceptar <a
+                href="{{ asset('assets/tyc-promocion-regreso-al-cole.pdf') }}" target="_blank"
+                rel="noopener noreferrer">Términos y Condiciones</a>
             @error('aceptar_tyc')
                 <span>{{ $message }}</span>
             @enderror
-    
-            <input type="checkbox" wire:model="aceptar_tratamiento_datos"> Aceptar Tratamiento de Datos
+
+            <input type="checkbox" wire:model="aceptar_tratamiento_datos"> Aceptar <a
+                href="https://privacy.newellbrands.com/index?language_id=4963328" target="_blank"
+                rel="noopener noreferrer">Tratamiento de Datos</a>
             @error('aceptar_tratamiento_datos')
                 <span>{{ $message }}</span>
             @enderror
-    
-            <button type="submit">Register</button>
+
+            <button type="submit">Enviar</button>
         </form>
     </div>
-    
+
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,4 @@
+<!-- filepath: /c:/laragon/www/regreso-al-cole/resources/views/welcome.blade.php -->
 <!DOCTYPE html>
 <html lang="en">
 
@@ -29,23 +30,24 @@
                 <div class="welcome-info-text">
                     <h1 class="welcome-title">¿Cómo participar?</h1>
                     <div class="welcome-info">
-                        <p>1. Regístrate.</p>
-                        <p>2. Sube la factura de compra con la foto de los productos participantes más tu dibujo.</p>
-                        <p>3. Podrás ser uno de los 150 ganadores semanales.</p>
+                        <p><img src="{{ asset('assets/registro1.png') }}" alt="Registro 1"> Regístrate.</p>
+                        <p><img src="{{ asset('assets/registro2.png') }}" alt="Registro 2"> Sube la factura de compra con la foto de los productos participantes más tu dibujo.</p>
+                        <p><img src="{{ asset('assets/registro3.png') }}" alt="Registro 3"> Podrás ser uno de los 150 ganadores semanales.</p>
                     </div>
                     <div class="welcome-btn-container">
-                        <a class="welcome-btn" id="entrar_btn" href="{{ route('login') }}">Entra</a>
+                        <div class="btn-with-image">
+                            <a class="welcome-btn" id="entrar_btn" href="{{ route('login') }}">Entra</a>
+                            <img src="{{ asset('assets/select-img.png') }}" alt="Select Image" class="select-img">
+                        </div>
                         <a class="welcome-btn" id="registrar_btn" href="{{ route('register') }}">Registrate</a>
                     </div>
                 </div>
                 <div class="welcome-premios-img">
                     <img src="{{ asset('assets/premios-home-dekstop.png') }}" alt="">
                 </div>
-
             </div>
         </div>
     </div>
-
 </body>
 
 </html>
