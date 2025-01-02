d14fee3fcfd1f7534029091fa34a6950e7e4a773
Neffer2 <n3f73r@gmail.com>
2025-01-02 10:47:33 -0500

    1. Avances vista backoffice

--- app/Livewire/Backoffice/Dashboard.php
+++ app/Livewire/Backoffice/Dashboard.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Backoffice;
 
+use App\Rules\num_factura;
 use Livewire\Component;
 use App\Models\User;
 use App\Models\RegistroFactura;
@@ -11,10 +12,71 @@ class Dashboard extends Component
 {
     use WithPagination;
 
+    // Models
+    public $RegistroFactura, $num_factura, $aprobacion_portada, $observaciones;
+
     public function render()
     {
         $RegistrosFactura = RegistroFactura::where('estado_id', 2)->paginate(5);
         return view('livewire.backoffice.dashboard', ['RegistrosFactura' => $RegistrosFactura]);
     }
 
+    public function getRegistro($registro_id)
+    {
+        $this->RegistroFactura = RegistroFactura::find($registro_id);
+    }
+
+    public function validacionRegistro($validacion)
+    {
+        $this->validate([
+            'num_factura' => ['required', 'alpha_num:ascii', new num_factura],
+            'aprobacion_portada' => ['required', 'numeric'],
+            'observaciones' => ['required', 'string']
+        ]);
+
+        if ($validacion){
+            $this->RegistroFactura->num_factura = $this->num_factura;
+            $this->RegistroFactura->estado_id = 1;
+            $this->RegistroFactura->estado_portada = $this->aprobacion_portada;
+            $this->RegistroFactura->observaciones = $this->observaciones;
+            $this->RegistroFactura->save();
+
+            $message = 'Factura APROBADA exitosamente.';
+        }else {
+            $this->RegistroFactura->num_factura = $this->num_factura;
+            $this->RegistroFactura->estado_id = 4;
+            $this->RegistroFactura->estado_portada = 4;
+            $this->RegistroFactura->observaciones = $this->observaciones;
+            $this->RegistroFactura->save();
+
+            $message = 'Factura RECHAZADA exitosamente.';
+        }
+
+        $this->reset(
+            'RegistroFactura',
+            'num_factura',
+            'aprobacion_portada',
+            'observaciones'
+        );
+        return redirect()->back()->with('success', $message);
+    }
+
+    // VALIDATIONS
+    public function updatedNumFactura(){
+        $this->validate([
+            'num_factura' => ['required', 'alpha_num:ascii', new num_factura]
+        ]);
+    }
+
+    public function updatedAprobacionPortada(){
+        $this->validate([
+            'aprobacion_portada' => ['required', 'numeric']
+        ]);
+    }
+
+    public function updatedObservaciones(){
+        $this->validate([
+            'observaciones' => ['required', 'string']
+        ]);
+    }
 }
--- resources/views/livewire/backoffice/dashboard.blade.php
+++ resources/views/livewire/backoffice/dashboard.blade.php
@@ -1,66 +1,129 @@
 <div class="container my-5">
+    @session('success')
+        <div class="alert alert-primary" role="alert">
+            {{ session('success') }}
+        </div>
+    @endsession
     <div class="card">
         <div class="card-header">
             <h5>Registro de compras</h5>
         </div>
+        @isset($RegistroFactura)
         <div class="card-body">
             <div class="card">
+                <div class="card-header">
+                    <div class="row">
+                        <div class="col-4">
+                            <span class="fw-bold">Usuario:</span>
+                            {{ $RegistroFactura->user->name }} <br>
+                            <span class="fw-bold">Correo:</span>
+                            {{ $RegistroFactura->user->email }}
+                        </div>
+                        <div class="col-4">
+                            <span class="fw-bold">Celular:</span>
+                            {{ $RegistroFactura->user->celular }} <br>
+                            <span class="fw-bold">Cedula:</span>
+                            {{ $RegistroFactura->user->cedula }}
+                        </div>
+                        <div class="col-4">
+                            <span class="fw-bold">Fecha:</span>
+                            {{ $RegistroFactura->created_at }}
+                        </div>
+                    </div>
+                </div>
                 <div class="card-body">
                     <div class="row">
                         <div class="col-3">
-                            <div class="form-group">
-                                <label for="">Foto de factura:</label>
-                                <input type="text" class="form-control">
-                            </div>
-                        </div>
-                        <div class="col-4">
-                            <div class="form-group d-flex flex-column">
-                                <label for="">N&uacute;mero de factura:</label>
-                                <a href="">
-                                    <img src="public/facturas/uCKQ7N1ME3mgBCbLI5Y8rzWpDl2jxpA780eBS8fV.jpg" height="250" width="250">
-                                    {{-- http://localhost:8000/storage/facturas/uCKQ7N1ME3mgBCbLI5Y8rzWpDl2jxpA780eBS8fV.jpg --}}
-                                </a>
+                            <div class="row">
+                                @php
+                                    $foto_factura = str_replace('public/', '', $RegistroFactura->foto_factura);
+                                    $foto_portada = str_replace('public/', '', $RegistroFactura->foto_portada);
+                                @endphp
+                                <div class="col-12">
+                                    <div class="form-group d-flex flex-column">
+                                        <label for="">Foto de factura:</label>
+                                        <a href="{{ asset("storage/$foto_factura") }}" target="_blank">
+                                            <img src="{{ asset("storage/$foto_factura") }}" height="250" width="250">
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="form-group">
+                                        <label for="">N&uacute;mero de factura:</label>
+                                        <input wire:model.lazy="num_factura" type="text" class="form-control">
+                                        @error('num_factura')
+                                            <span class="text-danger">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+                                </div>
                             </div>
                         </div>
                         <div class="col-3">
-                            <div class="form-group">
-                                <label for="">Foto de portada:</label>
-                                <input type="text" class="form-control">
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="form-group">
+                                        <label for="">Foto de portada:</label>
+                                        <a href="{{ asset("storage/$foto_portada") }}" target="_blank">
+                                            <img src="{{ asset("storage/$foto_portada") }}" height="250" width="250">
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="form-group">
+                                        <label for="">Aprobar portada</label>
+                                        <select wire:model.change="aprobacion_portada" name="" id="" class="form-control">
+                                            <option value="">Seleccionar</option>
+                                            <option value="1">Aprobar</option>
+                                            <option value="3">Rechazar</option>
+                                        </select>
+                                        @error('aprobacion_portada')
+                                            <span class="text-danger">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <div class="col-3">
-
-                        </div>
-                        <div class="col-2">
-                            {{-- <button class="btn btn-primary"> Ver mas </button> --}}
+                        <div class="col-6">
+                            <div class="form-group mb-2">
+                                <label for="">Observaciones:</label>
+                                <textarea wire:model.lazy="observaciones" cols="30" rows="5" class="form-control"></textarea>
+                                @error('observaciones')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+                            <button class="btn btn-success" wire:click="validacionRegistro(1)"
+                            wire:confirm="¿Estas segur@ de APROBAR esta factura?"> Aprobar factura</button>
+                            <button class="btn btn-danger" wire:click="validacionRegistro(0)"
+                            wire:confirm="¿Estas segur@ de RECHAZAR esta factura?"> Rechazar factura</button>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+        @endisset
         <div class="card-body">
-            @foreach ($RegistrosFactura as $RegistrosFactura)
+            @foreach ($RegistrosFactura as $RegistroFactura)
                 <div class="card my-2">
                     <div class="card-body">
                         <div class="row">
                             <div class="col-4">
                                 <span class="fw-bold">Usuario:</span>
-                                {{ $RegistrosFactura->user->name }} <br>
+                                {{ $RegistroFactura->user->name }} <br>
                                 <span class="fw-bold">Correo:</span>
-                                {{ $RegistrosFactura->user->email }}
+                                {{ $RegistroFactura->user->email }}
                             </div>
                             <div class="col-3">
                                 <span class="fw-bold">Celular:</span>
-                                {{ $RegistrosFactura->user->celular }} <br>
+                                {{ $RegistroFactura->user->celular }} <br>
                                 <span class="fw-bold">Cedula:</span>
-                                {{ $RegistrosFactura->user->cedula }}
+                                {{ $RegistroFactura->user->cedula }}
                             </div>
                             <div class="col-3">
                                 <span class="fw-bold">Fecha:</span>
-                                {{ $RegistrosFactura->created_at }}
+                                {{ $RegistroFactura->created_at }}
                             </div>
                             <div class="col-2">
-                                <button class="btn btn-primary"> Ver mas </button>
+                                <button wire:click="getRegistro({{ $RegistroFactura->id }})" class="btn btn-primary"> Ver mas </button>
                             </div>
                         </div>
                     </div>
