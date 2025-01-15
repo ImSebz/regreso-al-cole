48d8efbe102dd14c5d6897d23cf51f32d565751c
Sebastián Camargo <juansecapri@hotmail.com>
2025-01-15 14:56:21 -0500

    Ajustes de usuario descalificado

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,6 +57,7 @@ class Kernel extends HttpKernel
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
+        'descalificado' => \App\Http\Middleware\Descalificado::class,
         'tutor' => \App\Http\Middleware\Tutor::class,
         'backoffice' => \App\Http\Middleware\BackOffice::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
